faf4bcdc6c7f76199d3ab1686bdf82e1a59c3531
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-02-27 12:49:14 +0100

    Ytterligare en grid för discount code

--- WPFProjectAssignment/WPFProjectAssignment/MainWindow.xaml.cs
+++ WPFProjectAssignment/WPFProjectAssignment/MainWindow.xaml.cs
@@ -61,25 +61,45 @@ namespace WPFProjectAssignment
             Grid thirdGrid = new Grid();
             thirdGrid.ShowGridLines = true;
             thirdGrid.Margin = new Thickness(5);
+            // Top row for list of available products, Bottom row for shopping cart
+            thirdGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+            thirdGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
             
             // Fourth Grid
             Grid fourthGrid = new Grid();
             fourthGrid.ShowGridLines = true;
             fourthGrid.Margin = new Thickness(5);
-
-            // Left side for item list and shopping cart, right side for item description
-            thirdGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
-            thirdGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            // Left column for items in shoppingcart, right for checkout
+            fourthGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            fourthGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            
+            // Fifth Grid
+            Grid fifthGrid = new Grid();
+            fifthGrid.ShowGridLines = true;
+            fifthGrid.Margin = new Thickness(5);
+            // Left column for items in shoppingcart, right for checkout
+            fifthGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            fifthGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
             
             // add second grid to into second row of first grid
             firstgrid.Children.Add(secondGrid);
-            Grid.SetColumn(thirdGrid, 0);
+            Grid.SetColumn(secondGrid, 0);
             Grid.SetRow(secondGrid, 1);
             
             // add third grid to into first column of second grid
             secondGrid.Children.Add(thirdGrid);
             Grid.SetColumn(thirdGrid, 0);
             Grid.SetRow(thirdGrid, 1);
+            
+            //add fourth grid into third
+            thirdGrid.Children.Add(fourthGrid);
+            Grid.SetColumn(fourthGrid, 0);
+            Grid.SetRow(fourthGrid, 1);
+            
+            //add fourth grid into third
+            fourthGrid.Children.Add(fifthGrid);
+            Grid.SetColumn(fifthGrid, 1);
+            Grid.SetRow(fifthGrid, 0);
             
             
         }
