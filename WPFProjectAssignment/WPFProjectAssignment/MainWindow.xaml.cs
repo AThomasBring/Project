7a1a7cf38b1944cc5fc86975c4cb2f7bbd385c7a
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-03-01 17:35:16 +0100

    Ladda Csv osv

--- WPFProjectAssignment/WPFProjectAssignment/MainWindow.xaml.cs
+++ WPFProjectAssignment/WPFProjectAssignment/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +16,80 @@ using System.Windows.Shapes;
 
 namespace WPFProjectAssignment
 {
+    public class Product
+    {
+        public string Code;
+        public string Name;
+        public string Description;
+        public int Price;
+    }
+
     public partial class MainWindow : Window
     {
-        
-        private StackPanel productPanel;
+        // A static variable in the `Program` class is available in every method of that class. Effectively a "global" variable.
+
+        // An array of all the products available, loaded from "Products.csv".
+        public static Product[] Products;
+
+        // A shopping cart is a dictionary mapping a Product object to the number of copies of that product we have added.
+        //public static Dictionary<Product, int> Cart;
+
+        // We store product information in a CSV file in the project directory.
+        public const string ProductFilePath = "Products.csv";
+
+        // We store the saved shopping cart in a CSV file outside the project directory, because then it will not be overwritten everytime we start the program.
+        //public const string CartFilePath = @"C:\Windows\Temp\Cart.csv";
 
         public MainWindow()
         {
             InitializeComponent();
             Start();
         }
+        
+        public static Product[] LoadProducts()
+        {
+            // If the file doesn't exist, stop the program completely.
+            if (!File.Exists(ProductFilePath))
+            {
+                Console.WriteLine(ProductFilePath + " finns inte, eller har inte blivit satt till 'Copy Always'.");
+                Environment.Exit(1);
+            }
+
+            // Create an empty list of products, then go through each line of the file to fill it.
+            List<Product> products = new List<Product>();
+            string[] lines = File.ReadAllLines(ProductFilePath);
+            foreach (string line in lines)
+            {
+                try
+                {
+                    // First, split the line on commas (CSV means "comma-separated values").
+                    string[] parts = line.Split(',');
+
+                    // Then create a product with its values set to the different parts of the line.
+                    Product p = new Product
+                    {
+                        Code = parts[0],
+                        Name = parts[1],
+                        Description = parts[2],
+                        Price = int.Parse(parts[3])
+                    };
+                    products.Add(p);
+                }
+                catch
+                {
+                    Console.WriteLine("Fel vid inläsning av en produkt!");
+                }
+            }
+
+            // The method returns an array rather than a list (because the products are fixed after the program has started), so we need to convert it before returning.
+            return products.ToArray();
+        }
 
         private void Start()
         {
+
+            Products = LoadProducts();
+            
             
             // Window options
             Title = "Potion Shop";
@@ -82,6 +144,16 @@ namespace WPFProjectAssignment
             fifthGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
             fifthGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
             
+            Grid descriptionGrid = new Grid();
+            descriptionGrid.ShowGridLines = true;
+            descriptionGrid.Margin = new Thickness(5);
+
+            
+            descriptionGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(6, GridUnitType.Star) });
+            descriptionGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            descriptionGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            descriptionGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            
             //Adding grids to the grids
             
             // add second grid to into second row of first grid
@@ -94,6 +166,11 @@ namespace WPFProjectAssignment
             Grid.SetColumn(thirdGrid, 0);
             Grid.SetRow(thirdGrid, 1);
             
+            // add description grid to into second column of second grid
+            secondGrid.Children.Add(descriptionGrid);
+            Grid.SetColumn(descriptionGrid, 1);
+            Grid.SetRow(descriptionGrid, 1);
+            
             //add fourth grid into third
             thirdGrid.Children.Add(fourthGrid);
             Grid.SetColumn(fourthGrid, 0);
@@ -104,24 +181,8 @@ namespace WPFProjectAssignment
             Grid.SetColumn(fifthGrid, 1);
             Grid.SetRow(fifthGrid, 0);
             
-            secondGrid.Children.Add(CreateLayoutPanel());
-            Grid.SetColumn(CreateLayoutPanel(), 1);
-            Grid.SetRow(CreateLayoutPanel(), 0);
+  
             
-        }
-        //Har bara kopierat denna från Kitchensink programmet
-        private Grid CreateLayoutPanel()
-        {
-            // The main layout is a grid with two columns and four rows.
-            // All rows are sized to their content ("auto") except the second row, which takes up all the remaining space.
-            Grid grid = new Grid();
-            grid.ColumnDefinitions.Add(new ColumnDefinition());
-            grid.ColumnDefinitions.Add(new ColumnDefinition());
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            grid.RowDefinitions.Add(new RowDefinition());
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-
             // A text heading.
             TextBlock heading = new TextBlock
             {
@@ -132,23 +193,34 @@ namespace WPFProjectAssignment
                 FontSize = 20,
                 TextAlignment = TextAlignment.Center
             };
-            grid.Children.Add(heading);
+            firstgrid.Children.Add(heading);
             Grid.SetColumn(heading, 0);
             Grid.SetRow(heading, 0);
-            Grid.SetColumnSpan(heading, 2);
-
+            //Grid.SetColumnSpan(heading, 2);
+            
             // The list box allowing us to select a product.
             // Fills the left half of the second row.
             //Add code here to read products in from CSV file.
-            ListBox productBox = new ListBox { Margin = new Thickness(5) };
-            productBox.Items.Add("Product1");
-            productBox.Items.Add("Product2");
-            productBox.Items.Add("Product3");
-            productBox.SelectedIndex = 0;
-            grid.Children.Add(productBox);
-            Grid.SetColumn(productBox, 0);
-            Grid.SetRow(productBox, 1);
 
+            
+            ListBox productBox = new ListBox
+            {
+                Margin = new Thickness(5),
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
+
+            foreach (var product in Products)
+            {
+                productBox.Items.Add(product.Name);
+            }
+            productBox.SelectedIndex = 0;
+            thirdGrid.Children.Add(productBox);
+            Grid.SetColumn(productBox, 0);
+            Grid.SetRow(productBox, 0);
+            ScrollViewer viewer = new ScrollViewer();
+            viewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+            
             // The information panel describing a specific potion.
             // Fills the right half of the second row.
             StackPanel infoPanel = new StackPanel
@@ -156,9 +228,9 @@ namespace WPFProjectAssignment
                 Orientation = Orientation.Vertical,
                 Margin = new Thickness(5)
             };
-            grid.Children.Add(infoPanel);
-            Grid.SetColumn(infoPanel, 1);
-            Grid.SetRow(infoPanel, 1);
+            descriptionGrid.Children.Add(infoPanel);
+            Grid.SetColumn(infoPanel, 0);
+            Grid.SetRow(infoPanel, 0);
 
             // The text heading inside the information panel.
             TextBlock infoHeading = new TextBlock
@@ -197,15 +269,14 @@ namespace WPFProjectAssignment
                 Padding = new Thickness(5),
                 FontSize = 16
             };
-            grid.Children.Add(addToCartButton);
-            Grid.SetColumn(addToCartButton, 0);
+            descriptionGrid.Children.Add(addToCartButton);
+            Grid.SetColumn(addToCartButton, 1);
             Grid.SetRow(addToCartButton, 2);
             Grid.SetColumnSpan(addToCartButton, 2);
-
-
-            return grid;
+            
         }
-
+        
+        
         private Image CreateImage(string filePath)
         {
             ImageSource source = new BitmapImage(new Uri(filePath, UriKind.Relative));
