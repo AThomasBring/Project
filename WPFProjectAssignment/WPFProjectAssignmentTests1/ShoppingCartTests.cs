e4cc05ad467cbd2398fc5d2cb61fbfe119dcb239
AThomasBring <73478932+AThomasBring@users.noreply.github.com>
2021-03-10 13:55:23 +0000

    Create unit tests

---
+++ WPFProjectAssignment/WPFProjectAssignmentTests1/ShoppingCartTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFProjectAssignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WPFProjectAssignment.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void LoadFromFileTest()
+        {
+            string newTextFile = "abcd123";
+            File.WriteAllText(@"C:\Windows\Temp\TestFile.txt", newTextFile);
+
+            
+
+            Assert.AreEqual
+        }
+    }
+}
